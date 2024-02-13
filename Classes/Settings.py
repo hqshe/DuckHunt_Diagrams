8e7dd1dc7e07020265aa13a13149249a5b560107
ShulgaVlad <shulgavlad2112@gmail.com>
2024-02-14 01:44:31 +0200

    Settings class was filled with neccessary data

--- Classes/Settings.py
+++ Classes/Settings.py
@@ -1,7 +1,10 @@
 class Settings:
-    fps = 60
-    width = 900
-    height = 800
-    targets = {1: [10, 5, 3],
-           2: [12, 8, 5],
-           3: [15, 12, 8, 3]}
+    def __init__(self):
+        self.fps = 60
+        self.width = 900
+        self.height = 800
+        self.targets = {
+            1: [10, 5, 3],
+            2: [12, 8, 5],
+            3: [15, 12, 8, 3]
+        }
