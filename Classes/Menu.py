4417b2163b29c0a427583fd0d358acfc3fbc1032
hqshe <defadein@gmail.com>
2024-03-01 00:15:19 +0200

    menu draw function was added

--- Classes/Menu.py
+++ Classes/Menu.py
@@ -3,3 +3,14 @@ import pygame
 class Menu:
     def __init__(self, context):
         self.context = context
+
+    def draw(self):
+        if self.context.menu:
+            self.context.level = 0
+            self.draw_menu()
+        if self.context.game_over:
+            self.context.level = 0
+            self.draw_game_over()
+        if self.context.pause:
+            self.context.level = 0
+            self.draw_pause()
