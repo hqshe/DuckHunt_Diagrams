f015db3904ee820fe6b1cfab1610d10339892ed3
Vlad Buzilov <vladbuzilov23@gmail.com>
2024-02-29 23:26:32 +0200

    settings for game were added

--- Classes/Settings.py
+++ Classes/Settings.py
@@ -1,2 +1,10 @@
 class Settings:
-    pass
+    def __init__(self):
+        self.fps = 60
+        self.width = 900
+        self.height = 800
+        self.targets = {
+            1: [10, 5, 3],
+            2: [12, 8, 5],
+            3: [15, 12, 8, 3]
+        }
