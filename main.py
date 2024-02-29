8baaed22c995f5daff757f08e6c4d646759e9007
Vlad Shulga <shulgavlad2112@gmail.com>
2024-03-01 00:40:52 +0200

    main file got a part of his functional

--- main.py
+++ main.py
@@ -50,3 +50,4 @@ def main():
             context.three_coords = context.move_level()
             if context.shot:
                 context.three_coords = gun.check_shot(context.target_boxes, context.three_coords)
+                context.shot = False
