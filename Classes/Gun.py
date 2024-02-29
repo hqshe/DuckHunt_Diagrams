f46444e2f7582befa5eee09fa63c45b992d96d12
ShulgaVlad <shulgavlad2112@gmail.com>
2024-02-29 23:45:11 +0200

    check shot function was added

--- Classes/Gun.py
+++ Classes/Gun.py
@@ -33,3 +33,18 @@ class Gun:
                                          (self.context.WIDTH / 2 - 30, self.context.HEIGHT - 250))
                 if clicks[0]:
                     pygame.draw.circle(self.context.screen, lasers[self.context.level - 1], mouse_pos, 5)
+
+    def check_shot(self, targets, coords):
+        mouse_pos = pygame.mouse.get_pos()
+        for i in range(len(targets)):
+            for j in range(len(targets[i])):
+                if targets[i][j].collidepoint(mouse_pos):
+                    coords[i].pop(j)
+                    self.context.points += 10 + 10 * (i ** 2)
+                    if self.context.level == 1:
+                        self.context.bird_sound.play()
+                    elif self.context.level == 2:
+                        self.context.plate_sound.play()
+                    elif self.context.level == 3:
+                        self.context.laser_sound.play()
+        return coords
